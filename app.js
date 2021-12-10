1f362f37d23a8254c4857ca8531f588dcfc38095
heidichen90 <toddypet@gmail.com>
2021-12-10 13:15:02 +0000

    setup socket.io

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
-const passport = require('./config/passport')
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
+const httpServer = createServer(app);
+const port = process.env.PORT || 3000;
+const passport = require("./config/passport");
 
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(passport.initialize())
+const io = new Server(httpServer, {
+  cors: {
+    origin: [
+      "http://localhost:8080",
+      "http://localhost:8081",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowEI03: true,
+  },
+});
 
-app.listen(port, () =>
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+
+httpServer.listen(port, () =>
   console.log(`SimpleTwitter app listening on port ${port}!`)
-)
+);
 
-require('./routes')(app, passport)
-module.exports = app
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log(">>>>connection to server now");
+});
+
+require("./routes")(app, passport);
+module.exports = app;
+
+// socket.emit("connection", "I am online");
